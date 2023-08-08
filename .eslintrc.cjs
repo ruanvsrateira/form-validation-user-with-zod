058d7ffa5828286277719c6f2f12f3f7bdbd55e3
ruanvsrateira <vruan85@gmail.com>
2023-08-08 16:19:08 -0300

    chore(tailwind): setup tailwind

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,11 +4,27 @@ module.exports = {
     browser: true,
     es2020: true
   },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'plugin:storybook/recommended'],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:react-hooks/recommended", "eslint-config-prettier", "prettier", "plugin:storybook/recommended"],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'eslint-plugin-import-helpers'],
   rules: {
+    "@typescript-eslint/no-unused-vars": ["error"],
+    "semi": ["error", "always"],
+    "quotes": ["error", "single"],
+    "indent": ["error", 2, {
+      "SwitchCase": 1
+    }],
+    "space-in-parens": ["error", "never"],
+    "object-curly-spacing": [2, "always"],
+    "import-helpers/order-imports": ["warn", {
+      "newlinesBetween": "always",
+      "groups": [["/^react/"], ["module"], ["/^~//"], ["parent", "sibling", "index"]],
+      "alphabetize": {
+        "order": "asc",
+        "ignoreCase": true
+      }
+    }],
     'react-refresh/only-export-components': ['warn', {
       allowConstantExport: true
     }]
