4a035be0033f6bd70306514f6dbcff65852accb5
ruanvsrateira <vruan85@gmail.com>
2023-08-08 22:43:50 -0300

    chore(tailwind): add fonts and any colors to theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ['index.html', './src/**/*.{html,js,tss,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'Inter': ['Inter', 'sans-serif']
+      },
+      colors: {
+        'light-silver': '#A7A7A7', 
+        'dark-silver': '#343434'
+      }
+    },
   },
   plugins: [],
 }
