406ec41e348f06bbc18c0c4928b5a91e9e19bdc8
ruanvsrateira <vruan85@gmail.com>
2023-08-12 19:01:14 -0300

    feat(Component): Mount Design of Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { Fragment } from "react";
 import "./styles/global.scss";
+import { Form } from "./sections/Form/Form";
 
 export default function App() {
   return (
-    <Fragment>
-      <h1 className="text-black-200 text-lg">Componentes</h1>
-    </Fragment>
+    <div className="w-full h-[100vh] flex flex-col items-center justify-center gap-20">
+      <h1 className="text-4xl text-white font-Poppins text-center">
+        Cadastro de <span className="text-[#3ED869]">Usuário</span>
+      </h1>
+      <Form />
+    </div>
   );
 }
